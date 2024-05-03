9fbd2d0ddd98d892ce0e497bcaa0adeeea3384ad
awitamahari <awitamahari@gmail.com>
2024-05-03 09:18:59 +0200

    genre filtering function added

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,6 @@
 const apiKey = '4c426d7e09c045839ba9f733e886d4bf';
-async function fetchData(container, fetching_link) {
-  const endpoint = fetching_link;
-  
+// Fetching data based on the endpoint
+async function fetchData(container, endpoint) {
     fetch(endpoint)
       .then(response => {
         if (!response.ok) {
@@ -10,13 +9,19 @@ async function fetchData(container, fetching_link) {
         return response.json();
       })
       .then(data => {
-        insertData_into_container(data,container);
+        if(endpoint==`https://api.rawg.io/api/genres?key=${apiKey}`){
+          insertGenres_into_container(data,container);
+        }else{
+          console.log("endpoint:", endpoint , "data:", data);
+          insertData_into_container(data,container);
+        }
       })
       .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
     });
 }
 
+// Inserting the fetched data to its container
 function insertData_into_container(data,container) {
   container.innerHTML="";
   data.results.forEach(game => {
@@ -51,17 +56,49 @@ function insertData_into_container(data,container) {
   });
 }
 
+// Inserting the fetched genres to their container
+function insertGenres_into_container(data,container){
+  for (let index = 0; index < data.results.length; index++) {
+    const genre = data.results[index];
+    const active_genre=(index==0)?"active":"";
+    const genre_span=`<span class="Genre ${active_genre}">${genre.name}</span>`;
+    if(genre.name!=="RPG"){
+       container.innerHTML+=genre_span;
+    } 
+  }
+}
+
 let pageNumber=1;
 const topPopularGamesContainer = document.querySelector('.top-popular-games-container .games-list');
 const All_Games_Section = document.querySelector('.All_Games_Section .games-list');
+const Genre_Games = document.querySelector('.Genres-section .games-list');
+const Genres_container = document.querySelector('.Genres-section .Genres');
 
 document.addEventListener('DOMContentLoaded', function() {
+  let Genre="action";
   fetchData(topPopularGamesContainer, `https://api.rawg.io/api/games?key=${apiKey}`);
   fetchData(All_Games_Section, `https://api.rawg.io/api/games?key=${apiKey}&page=${pageNumber}`);
-});
+  fetchData(Genre_Games, `https://api.rawg.io/api/games?key=${apiKey}&genres=${Genre}`);
+  fetchData(Genres_container, `https://api.rawg.io/api/genres?key=${apiKey}`);
 
-fetchData(topPopularGamesContainer, `https://api.rawg.io/api/games?key=${apiKey}`);
-fetchData(All_Games_Section, `https://api.rawg.io/api/games?key=${apiKey}&page=${pageNumber}`);
+   // Attach click event listener to document
+   document.addEventListener('click', function(event) {
+    const clickedElement = event.target;
+    if (clickedElement.classList.contains('Genre')) {
+      let genre = clickedElement.textContent;
+      genre = genre.toLowerCase();
+       // Remove 'active' class from all genres
+       const genres_container = document.querySelectorAll(".Genres .Genre");
+       genres_container.forEach(genreElement => {
+           genreElement.classList.remove('active');
+       });
+
+       // Add 'active' class to clicked genre
+       clickedElement.classList.add('active');
+      fetchData(Genre_Games, `https://api.rawg.io/api/games?key=${apiKey}&genres=${genre}`);
+    }
+  });
+});
 
 let prev_page_btn=document.querySelector(".games_page_navigation .prev_btn");
 let next_page_btn=document.querySelector(".games_page_navigation .next_btn");
@@ -85,13 +122,6 @@ prev_page_btn.onclick=()=>{
   }
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
-
-/*
-let search_btn=document.querySelector('.search_container .search_btn');
-search_btn.onclick=()=>{
-  let search_input_value=document.querySelector('.search_container .search_input').value;
-  fetchData(All_Games_Section, `https://api.rawg.io/api/games?key=${apiKey}&search=${search_input_value}`);
-}*/
 
 let search_input=document.querySelector('.search_container .search_input');
 
